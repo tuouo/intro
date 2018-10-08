b9d0b3dc1c897bb62b19695cc84daae8bfd4af66
young luo <ltytuotuo@gmail.com>
2018-10-08 11:25:55 +0800

    template with template loader

--- django/intro/polls/views.py
+++ django/intro/polls/views.py
@@ -1,7 +1,16 @@
 from django.http import HttpResponse
+from django.template import loader
+
+from polls.models import Question
+
 
 def index(request):
-    return HttpResponse("Hello, django. Here's the polls index.")
+    template = loader.get_template('polls/index.html')
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {
+        'latest_question_list': latest_question_list,
+    }
+    return HttpResponse(template.render(context, request))
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question {}.".format(question_id))
